28712b5a75cbdf5c6dd998bc972996d72ae2b53a
Anton Ovechkin <owanturist@gmail.com>
2015-06-09 02:44:46 +0600

    Correct MultiWii.cpp;

--- MultiWii.cpp
+++ MultiWii.cpp
@@ -1,11 +1,19 @@
 /*
 * TOC
 *
+* RC ALIAS
+* AUTOMATIC ACC OFFSET CALIBRATION
+* POWER METER
+* RC FUNCTIONS
+* MOTOR AND SERVO FUNCTIONS
+* EEPROM LAYOUT DEFINITION
+* GPS COMMON VARIABLES
 * SETUP
 * GO ARM
 * GO DISARM
 * LOOP
 * - sticks command handler
+* - pitch & roll & yaw pid
 */
 
 #include <avr/io.h>
@@ -28,8 +36,7 @@
 
 #include <avr/pgmspace.h>
 
-/*********** RC alias *****************/
-
+// > RC ALIAS
 const char pidnames[] PROGMEM =
   "ROLL;"
   "PITCH;"
@@ -43,7 +50,8 @@ const char pidnames[] PROGMEM =
   "VEL;"
 ;
 
-const char boxnames[] PROGMEM = // names for dynamic generation of config GUI
+// names for dynamic generation of config GUI
+const char boxnames[] PROGMEM =
   "ARM;"
   #if ACC
     "ANGLE;"
@@ -94,7 +102,9 @@ const char boxnames[] PROGMEM = // names for dynamic generation of config GUI
   #endif
 ;
 
-const uint8_t boxids[] PROGMEM = {// permanent IDs associated to boxes. This way, you can rely on an ID number to identify a BOX function.
+// permanent IDs associated to boxes.
+// This way, you can rely on an ID number to identify a BOX function.
+const uint8_t boxids[] PROGMEM = {
   0, //"ARM;"
   #if ACC
     1, //"ANGLE;"
@@ -148,14 +158,23 @@ const uint8_t boxids[] PROGMEM = {// permanent IDs associated to boxes. This way
 
 uint32_t currentTime = 0;
 uint16_t previousTime = 0;
-uint16_t cycleTime = 0;     // this is the number in micro second to achieve a full loop, it can differ a little and is taken into account in the PID loop
-uint16_t calibratingA = 0;  // the calibration is done in the main loop. Calibrating decreases at each cycle down to 0, then we enter in a normal mode.
-uint16_t calibratingB = 0;  // baro calibration = get new ground pressure value
+// this is the number in micro second to achieve a full loop,
+// it can differ a little and is taken into account in the PID loop
+uint16_t cycleTime = 0;
+// the calibration is done in the main loop.
+// Calibrating decreases at each cycle down to 0,
+// then we enter in a normal mode.
+uint16_t calibratingA = 0;
+// baro calibration = get new ground pressure value
+uint16_t calibratingB = 0;
 uint16_t calibratingG;
-int16_t  magHold,headFreeModeHold; // [-180;+180]
-uint8_t  vbatMin = VBATNOMINAL;  // lowest battery voltage in 0.1V steps
+// [-180;+180]
+int16_t  magHold,headFreeModeHold;
+// lowest battery voltage in 0.1V steps
+uint8_t  vbatMin = VBATNOMINAL;
 uint8_t  rcOptions[CHECKBOXITEMS];
-int32_t  AltHold; // in cm
+// in cm
+int32_t  AltHold;
 int16_t  sonarAlt;
 int16_t  BaroPID = 0;
 int16_t  errorAltitudeI = 0;
@@ -181,33 +200,21 @@ int16_t  debug[4];
 
 flags_struct_t f;
 
-//for log
-#if defined(LOG_VALUES) || defined(LCD_TELEMETRY)
-  uint16_t cycleTimeMax = 0;       // highest ever cycle timen
-  uint16_t cycleTimeMin = 65535;   // lowest ever cycle timen
-  int32_t  BAROaltMax;             // maximum value
-  uint16_t GPS_speedMax = 0;       // maximum speed from gps
-  uint16_t powerValueMaxMAH = 0;
-#endif
-#if defined(LOG_VALUES) || defined(LCD_TELEMETRY) || defined(ARMEDTIMEWARNING) || defined(LOG_PERMANENT)
-  uint32_t armedTime = 0;
-#endif
-
 int16_t  i2c_errors_count = 0;
 int16_t  annex650_overrun_count = 0;
 
 
 
 #if defined(THROTTLE_ANGLE_CORRECTION)
-  int16_t throttleAngleCorrection = 0;  // correction of throttle in lateral wind,
-  int8_t  cosZ = 100;         // cos(angleZ)*100
+  // correction of throttle in lateral wind,
+  int16_t throttleAngleCorrection = 0;
+  // cos(angleZ)*100
+  int8_t  cosZ = 100;
 #endif
 
 
 
-// **********************
-//Automatic ACC Offset Calibration
-// **********************
+// > AUTOMATIC ACC OFFSET CALIBRATION
 #if defined(INFLIGHT_ACC_CALIBRATION)
   uint16_t InflightcalibratingA = 0;
   int16_t AccInflightCalibrationArmed;
@@ -216,32 +223,11 @@ int16_t  annex650_overrun_count = 0;
   uint16_t AccInflightCalibrationActive = 0;
 #endif
 
-// **********************
-// power meter
-// **********************
-#if defined(POWERMETER) || ( defined(LOG_VALUES) && (LOG_VALUES >= 3) )
-  uint32_t pMeter[PMOTOR_SUM + 1];  // we use [0:7] for eight motors,one extra for sum
-  uint8_t pMeterV;                  // dummy to satisfy the paramStruct logic in ConfigurationLoop()
-  uint32_t pAlarm;                  // we scale the eeprom value from [0:255] to this value we can directly compare to the sum in pMeter[6]
-  uint16_t powerValue = 0;          // last known current
-#endif
+// > POWER METER
 uint16_t intPowerTrigger1;
 
-// **********************
-// telemetry
-// **********************
-#if defined(LCD_TELEMETRY)
-  uint8_t telemetry = 0;
-  uint8_t telemetry_auto = 0;
-#endif
-#ifdef LCD_TELEMETRY_STEP
-  char telemetryStepSequence []  = LCD_TELEMETRY_STEP;
-  uint8_t telemetryStepIndex = 0;
-#endif
 
-// ******************
-// rc functions
-// ******************
+// > RC FUNCTIONS
 #define ROL_LO  (1<<(2*ROLL))
 #define ROL_CE  (3<<(2*ROLL))
 #define ROL_HI  (2<<(2*ROLL))
@@ -258,68 +244,65 @@ uint16_t intPowerTrigger1;
 int16_t failsafeEvents = 0;
 volatile int16_t failsafeCnt = 0;
 
-int16_t rcData[RC_CHANS];    // interval [1000;2000]
-int16_t rcSerial[8];         // interval [1000;2000] - is rcData coming from MSP
-int16_t rcCommand[4];        // interval [1000;2000] for THROTTLE and [-500;+500] for ROLL/PITCH/YAW
-uint8_t rcSerialCount = 0;   // a counter to select legacy RX when there is no more MSP rc serial data
-int16_t lookupPitchRollRC[5];// lookup table for expo & RC rate PITCH+ROLL
-int16_t lookupThrottleRC[11];// lookup table for expo & mid THROTTLE
-
-#if defined(SPEKTRUM)
-  volatile uint8_t  spekFrameFlags;
-  volatile uint32_t spekTimeLast;
-#endif
-
-#if defined(OPENLRSv2MULTI)
-  uint8_t pot_P,pot_I; // OpenLRS onboard potentiometers for P and I trim or other usages
-#endif
+// interval [1000;2000]
+int16_t rcData[RC_CHANS];
+// interval [1000;2000] - is rcData coming from MSP
+int16_t rcSerial[8];
+// interval [1000;2000] for THROTTLE and [-500;+500] for ROLL/PITCH/YAW
+int16_t rcCommand[4];
+// a counter to select legacy RX when there is no more MSP rc serial data
+uint8_t rcSerialCount = 0;
+// lookup table for expo & RC rate PITCH+ROLL
+int16_t lookupPitchRollRC[5];
+// lookup table for expo & mid THROTTLE
+int16_t lookupThrottleRC[11];
 
 
-// *************************
-// motor and servo functions
-// *************************
+// > MOTOR AND SERVO FUNCTIONS
 int16_t axisPID[3];
 int16_t motor[8];
 int16_t servo[8] = {1500,1500,1500,1500,1500,1500,1500,1000};
 
-// ************************
-// EEPROM Layout definition
-// ************************
+// > EEPROM LAYOUT DEFINITION
 static uint8_t dynP8[2], dynD8[2];
 
 global_conf_t global_conf;
 
 conf_t conf;
 
-#ifdef LOG_PERMANENT
-  plog_t plog;
-#endif
 
-// **********************
-// GPS common variables
-// **********************
-  int16_t  GPS_angle[2] = { 0, 0};                      // the angles that must be applied for GPS correction
-  int32_t  GPS_coord[2];
-  int32_t  GPS_home[2];
-  int32_t  GPS_hold[2];
-  uint8_t  GPS_numSat;
-  uint16_t GPS_distanceToHome;                          // distance to home  - unit: meter
-  int16_t  GPS_directionToHome;                         // direction to home - unit: degree
-  uint16_t GPS_altitude;                                // GPS altitude      - unit: meter
-  uint16_t GPS_speed;                                   // GPS speed         - unit: cm/s
-  uint8_t  GPS_update = 0;                              // a binary toogle to distinct a GPS position update
-  uint16_t GPS_ground_course = 0;                       //                   - unit: degree*10
-  uint8_t  GPS_Present = 0;                             // Checksum from Gps serial
-  uint8_t  GPS_Enable  = 0;
+// > GPS COMMON VARIABLES
+// the angles that must be applied for GPS correction
+int16_t  GPS_angle[2] = { 0, 0};
+int32_t  GPS_coord[2];
+int32_t  GPS_home[2];
+int32_t  GPS_hold[2];
+uint8_t  GPS_numSat;
+// distance to home - unit: meter
+uint16_t GPS_distanceToHome;
+// direction to home - unit: degree
+int16_t  GPS_directionToHome;
+// GPS altitude - unit: meter
+uint16_t GPS_altitude;
+// GPS speed - unit: cm/s
+uint16_t GPS_speed;
+// a binary toogle to distinct a GPS position update
+uint8_t  GPS_update = 0;
+// - unit: degree*10
+uint16_t GPS_ground_course = 0;
+// Checksum from Gps serial
+uint8_t  GPS_Present = 0;
+uint8_t  GPS_Enable  = 0;
 
-  // The desired bank towards North (Positive) or South (Negative) : latitude
-  // The desired bank towards East (Positive) or West (Negative)   : longitude
-  int16_t  nav[2];
-  int16_t  nav_rated[2];    //Adding a rate controller to the navigation to make it smoother
+// The desired bank towards North (Positive) or South (Negative) : latitude
+// The desired bank towards East (Positive) or West (Negative)   : longitude
+int16_t  nav[2];
+//Adding a rate controller to the navigation to make it smoother
+int16_t  nav_rated[2];
 
-  uint8_t nav_mode = NAV_MODE_NONE; // Navigation mode
+uint8_t nav_mode = NAV_MODE_NONE; // Navigation mode
 
-  uint8_t alarmArray[16];           // array
+uint8_t alarmArray[16];
  
 #if BARO
   int32_t baroPressure;
@@ -327,7 +310,9 @@ conf_t conf;
   int32_t baroPressureSum;
 #endif
 
-void annexCode() { // this code is excetuted at each loop and won't interfere with control loop if it lasts less than 650 microseconds
+// this code is excetuted at each loop and won't
+// interfere with control loop if it lasts less than 650 microseconds
+void annexCode() {
   static uint32_t calibratedAccTime;
   uint16_t tmp,tmp2;
   uint8_t axis,prop1,prop2;
@@ -336,7 +321,8 @@ void annexCode() { // this code is excetuted at each loop and won't interfere wi
   prop2 = 128; // prop2 was 100, is 128 now
   if (rcData[THROTTLE]>1500) { // breakpoint is fix: 1500
     if (rcData[THROTTLE]<2000) {
-      prop2 -=  ((uint16_t)conf.dynThrPID*(rcData[THROTTLE]-1500)>>9); //  /512 instead of /500
+      //  /512 instead of /500
+      prop2 -=  ((uint16_t)conf.dynThrPID*(rcData[THROTTLE]-1500)>>9);
     } else {
       prop2 -=  conf.dynThrPID;
     }
@@ -348,25 +334,34 @@ void annexCode() { // this code is excetuted at each loop and won't interfere wi
       if (tmp>DEADBAND) { tmp -= DEADBAND; }
       else { tmp=0; }
     #endif
-    if(axis!=2) { //ROLL & PITCH
+    if(axis!=2) {  //ROLL & PITCH
       tmp2 = tmp>>7; // 500/128 = 3.9  => range [0;3]
-      rcCommand[axis] = lookupPitchRollRC[tmp2] + ((tmp-(tmp2<<7)) * (lookupPitchRollRC[tmp2+1]-lookupPitchRollRC[tmp2])>>7);
-      prop1 = 128-((uint16_t)conf.rollPitchRate*tmp>>9); // prop1 was 100, is 128 now -- and /512 instead of /500
-      prop1 = (uint16_t)prop1*prop2>>7; // prop1: max is 128   prop2: max is 128   result prop1: max is 128
-      dynP8[axis] = (uint16_t)conf.pid[axis].P8*prop1>>7; // was /100, is /128 now
-      dynD8[axis] = (uint16_t)conf.pid[axis].D8*prop1>>7; // was /100, is /128 now
-    } else {      // YAW
+      rcCommand[axis] = lookupPitchRollRC[tmp2] + ((tmp-(tmp2<<7))
+                      * (lookupPitchRollRC[tmp2+1]-lookupPitchRollRC[tmp2])>>7);
+      // prop1 was 100, is 128 now -- and /512 instead of /500
+      prop1 = 128-((uint16_t)conf.rollPitchRate*tmp>>9);
+      // prop1: max is 128   prop2: max is 128   result prop1: max is 128
+      prop1 = (uint16_t)prop1*prop2>>7;
+      // was /100, is /128 now
+      dynP8[axis] = (uint16_t)conf.pid[axis].P8*prop1>>7;
+      // was /100, is /128 now
+      dynD8[axis] = (uint16_t)conf.pid[axis].D8*prop1>>7;
+    } else {  // YAW
       rcCommand[axis] = tmp;
     }
     if (rcData[axis]<MIDRC) rcCommand[axis] = -rcCommand[axis];
   }
   tmp = constrain(rcData[THROTTLE],MINCHECK,2000);
-  tmp = (uint32_t)(tmp-MINCHECK)*2559/(2000-MINCHECK); // [MINCHECK;2000] -> [0;2559]
+  // [MINCHECK;2000] -> [0;2559]
+  tmp = (uint32_t)(tmp-MINCHECK)*2559/(2000-MINCHECK);
   tmp2 = tmp/256; // range [0;9]
-  rcCommand[THROTTLE] = lookupThrottleRC[tmp2] + (tmp-tmp2*256) * (lookupThrottleRC[tmp2+1]-lookupThrottleRC[tmp2]) / 256; // [0;2559] -> expo -> [conf.minthrottle;MAXTHROTTLE]
+  // [0;2559] -> expo -> [conf.minthrottle;MAXTHROTTLE]
+  rcCommand[THROTTLE] = lookupThrottleRC[tmp2] + (tmp-tmp2*256)
+                      * (lookupThrottleRC[tmp2+1]-lookupThrottleRC[tmp2]) / 256;
 
   if(f.HEADFREE_MODE) { //to optimize
-    float radDiff = (att.heading - headFreeModeHold) * 0.0174533f; // where PI/180 ~= 0.0174533
+    // where PI/180 ~= 0.0174533
+    float radDiff = (att.heading - headFreeModeHold) * 0.0174533f;
     float cosDiff = cos(radDiff);
     float sinDiff = sin(radDiff);
     int16_t rcCommand_PITCH = rcCommand[PITCH]*cosDiff + rcCommand[ROLL]*sinDiff;
@@ -377,80 +372,8 @@ void annexCode() { // this code is excetuted at each loop and won't interfere wi
   // query at most one multiplexed analog channel per MWii cycle
   static uint8_t analogReader =0;
   switch (analogReader++%3) {
-  #if defined(POWERMETER_HARD)
-  case 0:
-  {
-    uint16_t pMeterRaw; // used for current reading
-    static uint32_t lastRead = currentTime;
-    static uint8_t ind = 0;
-    static uint16_t pvec[PSENSOR_SMOOTH], psum;
-    uint16_t p =  analogRead(PSENSORPIN);
-    //LCDprintInt16(p); LCDcrlf();
-    //debug[0] = p;
-    #if PSENSOR_SMOOTH != 1
-      psum += p;
-      psum -= pvec[ind];
-      pvec[ind++] = p;
-      ind %= PSENSOR_SMOOTH;
-      p = psum / PSENSOR_SMOOTH;
-    #endif
-    powerValue = ( conf.psensornull > p ? conf.psensornull - p : p - conf.psensornull); // do not use abs(), it would induce implicit cast to uint and overrun
-    analog.amperage = powerValue * conf.pint2ma;
-    pMeter[PMOTOR_SUM] += ((currentTime-lastRead) * (uint32_t)((uint32_t)powerValue*conf.pint2ma))/100000; // [10 mA * msec]
-    lastRead = currentTime;
-    break;
-  }
-  #endif // POWERMETER_HARD
 
-  #if defined(VBAT)
-  case 1:
-  {
-      static uint8_t ind = 0;
-      static uint16_t vvec[VBAT_SMOOTH], vsum;
-      uint16_t v = analogRead(V_BATPIN);
-      //debug[1] = v;
-      #if VBAT_SMOOTH == 1
-        analog.vbat = (v<<4) / conf.vbatscale; // result is Vbatt in 0.1V steps
-      #else
-        vsum += v;
-        vsum -= vvec[ind];
-        vvec[ind++] = v;
-        ind %= VBAT_SMOOTH;
-        #if VBAT_SMOOTH == 16
-          analog.vbat = vsum / conf.vbatscale; // result is Vbatt in 0.1V steps
-        #elif VBAT_SMOOTH < 16
-          analog.vbat = (vsum * (16/VBAT_SMOOTH)) / conf.vbatscale; // result is Vbatt in 0.1V steps
-        #else
-          analog.vbat = ((vsum /VBAT_SMOOTH) * 16) / conf.vbatscale; // result is Vbatt in 0.1V steps
-        #endif
-      #endif
-      break;
-  }
-  #endif // VBAT
-  #if defined(RX_RSSI)
-  case 2:
-  {
-    static uint8_t ind = 0;
-    static uint16_t rvec[RSSI_SMOOTH], rsum;
-    uint16_t r = analogRead(RX_RSSI_PIN);
-    #if RSSI_SMOOTH == 1
-      analog.rssi = r;
-    #else
-      rsum += r;
-      rsum -= rvec[ind];
-      rvec[ind++] = r;
-      ind %= RSSI_SMOOTH;
-      r = rsum / RSSI_SMOOTH;
-      analog.rssi = r;
-    #endif
-    break;
-  }
-  #endif
   } // end of switch()
-
-  #if defined(BUZZER)
-    alarmHandler(); // external buzzer routine that handles buzzer events globally now
-  #endif
 
 
   if ( (calibratingA>0 && ACC ) || (calibratingG>0) ) { // Calibration phasis
@@ -460,17 +383,7 @@ void annexCode() { // this code is excetuted at each loop and won't interfere wi
     if (f.ARMED) {LEDPIN_ON;}
   }
 
-  #if defined(LED_RING)
-    static uint32_t LEDTime;
-    if ( currentTime > LEDTime ) {
-      LEDTime = currentTime + 50000;
-      i2CLedRingState();
-    }
-  #endif
 
-  #if defined(LED_FLASHER)
-    auto_switch_led_flasher();
-  #endif
 
   if ( currentTime > calibratedAccTime ) {
     if (! f.SMALL_ANGLES_25) {
@@ -483,57 +396,14 @@ void annexCode() { // this code is excetuted at each loop and won't interfere wi
     }
   }
 
-  #if !(defined(SPEKTRUM) && defined(PROMINI))  //Only one serial port on ProMini.  Skip serial com if Spektrum Sat in use. Note: Spek code will auto-call serialCom if GUI data detected on serial0.
-    #if defined(GPS_PROMINI)
-      if(GPS_Enable == 0) {serialCom();}
-    #else
-      serialCom();
-    #endif
+  //Only one serial port on ProMini.
+  //Skip serial com if Spektrum Sat in use.
+  //Note: Spek code will auto-call serialCom if GUI data detected on serial0.
+  #if !(defined(SPEKTRUM) && defined(PROMINI))
+    serialCom();
   #endif
 
-  #if defined(POWERMETER)
-    analog.intPowerMeterSum = (pMeter[PMOTOR_SUM]/PLEVELDIV);
-    intPowerTrigger1 = conf.powerTrigger1 * PLEVELSCALE; 
-  #endif
 
-  #ifdef LCD_TELEMETRY_AUTO
-    static char telemetryAutoSequence []  = LCD_TELEMETRY_AUTO;
-    static uint8_t telemetryAutoIndex = 0;
-    static uint16_t telemetryAutoTimer = 0;
-    if ( (telemetry_auto) && (! (++telemetryAutoTimer % LCD_TELEMETRY_AUTO_FREQ) )  ){
-      telemetry = telemetryAutoSequence[++telemetryAutoIndex % strlen(telemetryAutoSequence)];
-      LCDclear(); // make sure to clear away remnants
-    }
-  #endif  
-  #ifdef LCD_TELEMETRY
-    static uint16_t telemetryTimer = 0;
-    if (! (++telemetryTimer % LCD_TELEMETRY_FREQ)) {
-      #if (LCD_TELEMETRY_DEBUG+0 > 0)
-        telemetry = LCD_TELEMETRY_DEBUG;
-      #endif
-      if (telemetry) lcd_telemetry();
-    }
-  #endif
-
-  #if GPS & defined(GPS_LED_INDICATOR)       // modified by MIS to use STABLEPIN LED for number of sattelites indication
-    static uint32_t GPSLEDTime;              // - No GPS FIX -> LED blink at speed of incoming GPS frames
-    static uint8_t blcnt;                    // - Fix and sat no. bellow 5 -> LED off
-    if(currentTime > GPSLEDTime) {           // - Fix and sat no. >= 5 -> LED blinks, one blink for 5 sat, two blinks for 6 sat, three for 7 ...
-      if(f.GPS_FIX && GPS_numSat >= 5) {
-        if(++blcnt > 2*GPS_numSat) blcnt = 0;
-        GPSLEDTime = currentTime + 150000;
-        if(blcnt >= 10 && ((blcnt%2) == 0)) {STABLEPIN_ON;} else {STABLEPIN_OFF;}
-      }else{
-        if((GPS_update == 1) && !f.GPS_FIX) {STABLEPIN_ON;} else {STABLEPIN_OFF;}
-        blcnt = 0;
-      }
-    }
-  #endif
-
-  #if defined(LOG_VALUES) && (LOG_VALUES >= 2)
-    if (cycleTime > cycleTimeMax) cycleTimeMax = cycleTime; // remember highscore
-    if (cycleTime < cycleTimeMin) cycleTimeMin = cycleTime; // remember lowscore
-  #endif
   if (f.ARMED)  {
     #if defined(LCD_TELEMETRY) || defined(ARMEDTIMEWARNING) || defined(LOG_PERMANENT)
       armedTime += (uint32_t)cycleTime;
