47d5fa95a6d6715c839f8d1a7ac9e8e4074e0246
Anton Ovechkin <owanturist@gmail.com>
2015-06-12 12:41:19 +0600

    Formated Serial;

--- Serial.cpp
+++ Serial.cpp
@@ -10,39 +10,45 @@ static volatile uint8_t serialHeadTX[UART_NUMBER],serialTailTX[UART_NUMBER];
 static uint8_t serialBufferTX[TX_BUFFER_SIZE][UART_NUMBER];
 
 
-// *******************************************************
-// Interrupt driven UART transmitter - using a ring buffer
-// *******************************************************
+/**
+ * Interrupt driven UART transmitter - using a ring buffer
+ */
 
-
-ISR(USART_UDRE_vect) {  // Serial 0 on a PROMINI
+ISR(USART_UDRE_vect) {   // Serial 0 on a PROMINI
   uint8_t t = serialTailTX[0];
   if (serialHeadTX[0] != t) {
-    if (++t >= TX_BUFFER_SIZE) t = 0;
+    if (++t >= TX_BUFFER_SIZE) {
+      t = 0;
+    }
+
     UDR0 = serialBufferTX[t][0];  // Transmit next byte in the ring
     serialTailTX[0] = t;
   }
-  if (t == serialHeadTX[0]) UCSR0B &= ~(1<<UDRIE0); // Check if all data is transmitted . if yes disable transmitter UDRE interrupt
+  if (t == serialHeadTX[0]) {
+    // Check if all data is transmitted.
+    // If yes disable transmitter UDRE interrupt
+    UCSR0B &= ~(1 << UDRIE0);
+  }
 }
 
 void UartSendData(uint8_t port) {
-  UCSR0B |= (1<<UDRIE0);
+  UCSR0B |= (1 << UDRIE0);
 }
 
 void SerialOpen(uint8_t port, uint32_t baud) {
   uint8_t h = ((F_CPU  / 4 / baud -1) / 2) >> 8;
   uint8_t l = ((F_CPU  / 4 / baud -1) / 2);
-  UCSR0A  = (1<<U2X0);
+  UCSR0A  = (1 << U2X0);
   UBRR0H = h;
   UBRR0L = l;
-  UCSR0B |= (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
+  UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
 }
 
 void SerialEnd(uint8_t port) {
-  UCSR0B &= ~((1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<UDRIE0));
+  UCSR0B &= ~( (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0) | (1 << UDRIE0) );
 }
 
-void store_uart_in_buf(uint8_t data, uint8_t portnum) {
+void storeUartInBuf(uint8_t data, uint8_t portnum) {
   uint8_t h = serialHeadRX[portnum];
   if (++h >= RX_BUFFER_SIZE) h = 0;
   if (h == serialTailRX[portnum]) return; // we did not bite our own tail?
@@ -50,7 +56,9 @@ void store_uart_in_buf(uint8_t data, uint8_t portnum) {
   serialHeadRX[portnum] = h;
 }
 
-ISR(USART_RX_vect)  { store_uart_in_buf(UDR0, 0); }
+ISR(USART_RX_vect) {
+  storeUartInBuf(UDR0, 0);
+}
 
 uint8_t SerialRead(uint8_t port) {
   uint8_t t = serialTailRX[port];
@@ -64,11 +72,11 @@ uint8_t SerialRead(uint8_t port) {
 
 
 uint8_t SerialAvailable(uint8_t port) {
-  return ((uint8_t)(serialHeadRX[port] - serialTailRX[port]))%RX_BUFFER_SIZE;
+  return ( (uint8_t)(serialHeadRX[port] - serialTailRX[port]) ) % RX_BUFFER_SIZE;
 }
 
 uint8_t SerialUsedTXBuff(uint8_t port) {
-  return ((uint8_t)(serialHeadTX[port] - serialTailTX[port]))%TX_BUFFER_SIZE;
+  return ( (uint8_t)(serialHeadTX[port] - serialTailTX[port]) ) % TX_BUFFER_SIZE;
 }
 
 void SerialSerialize(uint8_t port,uint8_t a) {
@@ -79,5 +87,6 @@ void SerialSerialize(uint8_t port,uint8_t a) {
 }
 
 void SerialWrite(uint8_t port,uint8_t c){
-  SerialSerialize(port,c);UartSendData(port);
+  SerialSerialize(port,c);
+  UartSendData(port);
 }
