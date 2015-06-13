8c6fac388c565e8704e2200f5092579d528a232e
Anton Ovechkin <owanturist@gmail.com>
2015-06-13 21:46:24 +0600

    Formated RX;

--- RX.cpp
+++ RX.cpp
@@ -1,16 +1,3 @@
-/*
-* TOC
-*
-* GLOBAL RX RELATED VARIABLES
-* RX PIN SETUP
-* - Configure each rc pin for PCINT
-* - atmega328P's Specific Aux2 Pin Setup
-* STANDARD RX PINS READING
-* PORT CHANGE INTERRUPT
-* COMBINE AND SORT THE RX DATAS
-* COMPUTE AND FILTER THE RX DATA
-*/
-
 #include "Arduino.h"
 #include "config.h"
 #include "def.h"
@@ -20,74 +7,51 @@
 #include "MultiWii.h"
 
 
-// > GLOBAL RX RELATED VARIABLES
+// GLOBAL RX RELATED VARIABLES
 
 // RAW RC values will be store here
 // interval [1000;2000]
-volatile uint16_t rcValue[RC_CHANS] = {1502, 1502, 1502, 1502, 1502, 1502, 1502, 1502};
+volatile uint16_t rcValue[RC_CHANS] = {
+  1502, 1502, 1502, 1502,
+  1502, 1502, 1502, 1502
+};
 
 // Standard Channel order
 static uint8_t rcChannel[RC_CHANS]  = {   \
   ROLLPIN, PITCHPIN, YAWPIN, THROTTLEPIN, \
-  AUX1PIN,AUX2PIN,AUX3PIN,AUX4PIN};
-// if this slowes the PCINT readings we can switch to a define for each pcint bit
+  AUX1PIN,AUX2PIN,AUX3PIN,AUX4PIN         \
+};
+
+// if this slowes the PCINT readings we'
+// can switch to a define for each pcint bit
 static uint8_t PCInt_RX_Pins[PCINT_PIN_COUNT] = {PCINT_RX_BITS};
 
 void rxInt(void);
 
-// > RX PIN SETUP
+// RX PIN SETUP
 void configureReceiver() {
-  // >> Configure each rc pin for PCINT
+  // Configure each rc pin for PCINT
   // PCINT activation
-  // i think a for loop is ok for the init.
   for(uint8_t i = 0; i < PCINT_PIN_COUNT; i++){
     PCINT_RX_PORT |= PCInt_RX_Pins[i];
     PCINT_RX_MASK |= PCInt_RX_Pins[i];
   }
   PCICR = PCIR_PORT_BIT;
-  
-  // >> atmega328P's Specific Aux2 Pin Setup
- #if defined(RCAUXPIN)
-    PCICR  |= (1 << 0) ; // PCINT activated also for PINS [D8-D13] on port B
-    #if defined(RCAUXPIN8)
-      PCMSK0 = (1 << 0);
-    #endif
-    #if defined(RCAUXPIN12)
-      PCMSK0 = (1 << 4);
-    #endif
-  #endif
 }
 
-// > STANDARD RX PINS READING
-#if defined(FAILSAFE)
-   // predefined PC pin block (thanks to lianj)  - Version with failsafe
-  #define RX_PIN_CHECK(pin_pos, rc_value_pos)                        \
-    if (mask & PCInt_RX_Pins[pin_pos]) {                             \
-      if (!(pin & PCInt_RX_Pins[pin_pos])) {                         \
-        dTime = cTime-edgeTime[pin_pos];                             \
-        if (900<dTime && dTime<2200) {                               \
-          rcValue[rc_value_pos] = dTime;                             \
-          if((rc_value_pos==THROTTLEPIN || rc_value_pos==YAWPIN ||   \
-              rc_value_pos==PITCHPIN || rc_value_pos==ROLLPIN)       \
-              && dTime>FAILSAFE_DETECT_TRESHOLD)                     \
-                GoodPulses |= (1<<rc_value_pos);                     \
-        }                                                            \
-      } else edgeTime[pin_pos] = cTime;                              \
-    }
-#else
-   // predefined PC pin block (thanks to lianj)  - Version without failsafe
-  #define RX_PIN_CHECK(pin_pos, rc_value_pos)                        \
-    if (mask & PCInt_RX_Pins[pin_pos]) {                             \
-      if (!(pin & PCInt_RX_Pins[pin_pos])) {                         \
-        dTime = cTime-edgeTime[pin_pos];                             \
-        if (900<dTime && dTime<2200) {                               \
-          rcValue[rc_value_pos] = dTime;                             \
-        }                                                            \
-      } else edgeTime[pin_pos] = cTime;                              \
-    }
-#endif
+// STANDARD RX PINS READING
+// predefined PC pin block (thanks to lianj)  - Version without failsafe
+#define RX_PIN_CHECK(pin_pos, rc_value_pos)                        \
+  if (mask & PCInt_RX_Pins[pin_pos]) {                             \
+    if (!(pin & PCInt_RX_Pins[pin_pos])) {                         \
+      dTime = cTime-edgeTime[pin_pos];                             \
+      if (900<dTime && dTime<2200) {                               \
+        rcValue[rc_value_pos] = dTime;                             \
+      }                                                            \
+    } else edgeTime[pin_pos] = cTime;                              \
+  }
 
-// > PORT CHANGE INTERRUPT
+// PORT CHANGE INTERRUPT
 // this ISR is common to every receiver channel,
 // it is call everytime a change state occurs on a RX input pin
 ISR(RX_PC_INTERRUPT) {
@@ -96,10 +60,6 @@ ISR(RX_PC_INTERRUPT) {
   uint16_t cTime,dTime;
   static uint16_t edgeTime[8];
   static uint8_t PCintLast;
-
-  #if defined(FAILSAFE)
-    static uint8_t GoodPulses;
-  #endif
 
   // RX_PCINT_PIN_PORT indicates the state of each
   // PIN for the arduino port dealing with Ports digital pins
@@ -144,59 +104,53 @@ ISR(RX_PC_INTERRUPT) {
   #if (PCINT_PIN_COUNT > 7)
     RX_PIN_CHECK(7,3);
   #endif
-
-  #if defined(FAILSAFE)
-    // If all main four chanells have good pulses, clear FailSafe counter
-    if (GoodPulses==(1<<THROTTLEPIN)+(1<<YAWPIN)+(1<<ROLLPIN)+(1<<PITCHPIN)) {
-      GoodPulses = 0;
-      if(failsafeCnt > 20) failsafeCnt -= 20; else failsafeCnt = 0; 
-    }
-  #endif
 }
 
 
-// > COMBINE AND SORT THE RX DATAS
+// COMBINE AND SORT THE RX DATAS
 uint16_t readRawRC(uint8_t chan) {
   uint16_t data;
   uint8_t oldSREG;
-  oldSREG = SREG; cli(); // Let's disable interrupts
-  data = rcValue[rcChannel[chan]]; // Let's copy the data Atomically
-  SREG = oldSREG;        // Let's restore interrupt state
-  return data; // We return the value correctly copied when the IRQ's where disabled
+  oldSREG = SREG; cli();              // Let's disable interrupts
+  data = rcValue[rcChannel[chan]];    // Let's copy the data Atomically
+  SREG = oldSREG;                     // Let's restore interrupt state
+  // We return the value correctly copied when the IRQ's where disabled
+  return data;
 }
 
-// > COMPUTE AND FILTER THE RX DATA
+// COMPUTE AND FILTER THE RX DATA
 void computeRC() {
-  static uint16_t rcData4Values[RC_CHANS][4], rcDataMean[RC_CHANS];
+  static uint16_t rcData4Values[RC_CHANS][4];
+  static uint16_t rcDataMean[RC_CHANS];
   static uint8_t rc4ValuesIndex = 0;
-  uint8_t chan,a;
-  rc4ValuesIndex++;
-  if (rc4ValuesIndex == 4) rc4ValuesIndex = 0;
-  for (chan = 0; chan < RC_CHANS; chan++) {
-    #if defined(FAILSAFE)
-      uint16_t rcval = readRawRC(chan);
-      // update controls channel only if pulse is above FAILSAFE_DETECT_TRESHOLD
-      if(rcval>FAILSAFE_DETECT_TRESHOLD || chan > 3 || !f.ARMED) {
-        // In disarmed state allow always update for easer configuration.
-        rcData4Values[chan][rc4ValuesIndex] = rcval;
-      }
-    #else
-      rcData4Values[chan][rc4ValuesIndex] = readRawRC(chan);
-    #endif
 
+  if (++rc4ValuesIndex == 4) {
+    rc4ValuesIndex = 0;
+  }
+
+  for (uint8_t chan = 0; chan < RC_CHANS; chan++) {
+    rcData4Values[chan][rc4ValuesIndex] = readRawRC(chan);
     rcDataMean[chan] = 0;
-    for (a=0;a<4;a++) rcDataMean[chan] += rcData4Values[chan][a];
+
+    for (uint8_t a=0;a<4;a++) {
+      rcDataMean[chan] += rcData4Values[chan][a];
+    }
+
     rcDataMean[chan]= (rcDataMean[chan]+2)>>2;
-    if ( rcDataMean[chan] < (uint16_t)rcData[chan] -3)  rcData[chan] = rcDataMean[chan]+2;
-    if ( rcDataMean[chan] > (uint16_t)rcData[chan] +3)  rcData[chan] = rcDataMean[chan]-2;
+
+    if ( rcDataMean[chan] < (uint16_t)rcData[chan] - 3) {
+      rcData[chan] = rcDataMean[chan] + 2;
+    } else if ( rcDataMean[chan] > (uint16_t)rcData[chan] + 3) {
+      rcData[chan] = rcDataMean[chan] - 2;
+    }
+
     // rcData comes from MSP and overrides RX Data until rcSerialCount reaches 0
-    if (chan<8 && rcSerialCount > 0) {
+    if (chan < 8 && rcSerialCount > 0) {
       rcSerialCount --;
-      #if defined(FAILSAFE)
-        failsafeCnt = 0;
-      #endif
       // only relevant channels are overridden
-      if (rcSerial[chan] >900) {rcData[chan] = rcSerial[chan];}
+      if (rcSerial[chan] > 900) {
+        rcData[chan] = rcSerial[chan];
+      }
     }
   }
 }
